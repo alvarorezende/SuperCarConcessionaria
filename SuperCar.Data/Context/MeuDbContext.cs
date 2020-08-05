e34c413871330a038bd88b578aa970420b7b70ca
Alvaro Rezende <alvarorezende@momentum.com.br>
2020-08-05 10:54:50 -0300

    Mapping de entidades, criação da Migration Initial, criação de Script do Banco de Dados.

--- SuperCar.Data/Context/MeuDbContext.cs
+++ SuperCar.Data/Context/MeuDbContext.cs
@@ -1,17 +1,39 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Text;
+using SuperCar.Business.Models;
+using System.Linq;
 
 namespace SuperCar.Data.Context
 {
-	public class MeuDbContext : DbContext
-	{
-		public MeuDbContext(DbContextOptions options) : base(options)
-		{
+    public class MeuDbContext : DbContext
+    {
+        public MeuDbContext(DbContextOptions options) : base(options)
+        {
 
-		}
+        }
 
-		
-	}
+        public DbSet<Veiculo> Veiculos { get; set; }
+
+        public DbSet<Combustivel> Combustiveis { get; set; }
+
+        public DbSet<Cor> Cores { get; set; }
+
+        public DbSet<Marca> Marcas { get; set; }
+
+        public DbSet<Cambio> Cambios { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            foreach (var property in modelBuilder.Model.GetEntityTypes()
+                .SelectMany(e => e.GetProperties()
+                .Where(p => p.ClrType == typeof(string))))
+                property.Relational().ColumnType = "varchar(200)";
+
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(MeuDbContext).Assembly);
+
+            foreach (var relationship in modelBuilder.Model.GetEntityTypes()
+                .SelectMany(e => e.GetForeignKeys())) relationship.DeleteBehavior = DeleteBehavior.ClientSetNull;
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
 }
---
+++ SuperCar.Data/Mappings/VeiculoMapping.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SuperCar.Business.Models;
+
+namespace SuperCar.Data.Mappings
+{
+    public class VeiculoMapping : IEntityTypeConfiguration<Veiculo>
+    { 
+        public void Configure(EntityTypeBuilder<Veiculo> builder)
+        {
+            builder.HasKey(p => p.VeiculoId);
+
+            builder.Property(p => p.Placa)
+                .IsRequired()
+                .HasColumnType("varchar(7)");
+
+            builder.Property(p => p.Modelo)
+                .IsRequired()
+                .HasColumnType("varchar(200)");
+
+            builder.Property(p => p.ObsVeiculo)
+                .HasColumnType("varchar(1000)");
+
+            builder.HasOne(c => c.Cor)
+                .WithMany()
+                .HasForeignKey("CorId");
+
+            builder.HasOne(m => m.Marca)
+                .WithMany()
+                .HasForeignKey("MarcaId");
+
+            builder.HasOne(c => c.Cambio)
+                .WithMany()
+                .HasForeignKey("CambioId");
+
+            builder.HasOne(c => c.Combustivel)
+                .WithMany()
+                .HasForeignKey("CombustivelId");
+
+            builder.ToTable("Veiculos");
+        }
+    }
+
+    public class MarcaMapping : IEntityTypeConfiguration<Marca>
+    {
+        public void Configure(EntityTypeBuilder<Marca> builder)
+        {
+            builder.HasKey(p => p.MarcaId);
+
+            builder.Property(p => p.NomeMarca)
+                .IsRequired()
+                .HasColumnType("varchar(200)");
+
+            builder.ToTable("Marcas");
+        }
+    }
+
+    public class CambioMapping : IEntityTypeConfiguration<Cambio>
+    {
+        public void Configure(EntityTypeBuilder<Cambio> builder)
+        {
+            builder.HasKey(p => p.CambioId);
+
+            builder.Property(p => p.TipoCambio)
+                .IsRequired()
+                .HasColumnType("varchar(200)");
+
+            builder.ToTable("Cambios");
+        }
+    }
+
+    public class CorMapping : IEntityTypeConfiguration<Cor>
+    {
+        public void Configure(EntityTypeBuilder<Cor> builder)
+        {
+            builder.HasKey(p => p.CorId);
+
+            builder.Property(p => p.NomeCor)
+                .IsRequired()
+                .HasColumnType("varchar(200)");
+
+            builder.ToTable("Cores");
+        }
+    }
+
+    public class CombustivelMapping : IEntityTypeConfiguration<Combustivel>
+    {
+        public void Configure(EntityTypeBuilder<Combustivel> builder)
+        {
+            builder.HasKey(p => p.CombustivelId);
+
+            builder.Property(p => p.TipoCombustivel)
+                .IsRequired()
+                .HasColumnType("varchar(200)");
+
+            builder.ToTable("Combustiveis");
+        }
+    }
+}
